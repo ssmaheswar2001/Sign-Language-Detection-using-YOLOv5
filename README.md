703650579f6eafc56b969afbf5e798de5c176bcf
ssmaheswar2001 <ssmaheswar2001@gmail.com>
2024-10-23 15:03:05 -0500

    Requirements added.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Sign-Language-Detection-using-YOLOv5
+
+
+
+## How to run :
+
+```bash
+conda create -n singlang python=3.7 -y
+```
+
+```bash
+conda activate signlang
+```
+
+```bash
+pip install -r requirements.txt
+```
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'ssmaheswar',
+    author_email= 'ssmaheswar2001@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
