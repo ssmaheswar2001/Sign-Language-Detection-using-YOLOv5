62fb8e9ad056c48b38a6aeb31cd7db3c543fee00
ssmaheswar2001 <ssmaheswar2001@gmail.com>
2024-10-31 19:28:34 -0500

    Data Validation Added.

--- signLanguage/constant/training_pipeline/__init__.py
+++ signLanguage/constant/training_pipeline/__init__.py
@@ -11,3 +11,14 @@ DATA_INGESTION_DIR_NAME: str = "data_ingestion"
 DATA_INGESTION_FEATURE_STORE_DIR: str = "feature_store"
 
 DATA_DOWNLOAD_URL: str = "https://github.com/ssmaheswar2001/Sign-Language-Detection-using-YOLOv5/raw/refs/heads/main/Sign_language_data.zip"
+
+
+"""
+Data Validation realted contant start with DATA_VALIDATION VAR NAME
+"""
+
+DATA_VALIDATION_DIR_NAME: str = "data_validation"
+
+DATA_VALIDATION_STATUS_FILE = 'status.txt'
+
+DATA_VALIDATION_ALL_REQUIRED_FILES = ["train", "test", "data.yaml"]
